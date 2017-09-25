bfea45bc55dc0f6a2c61c918d6f0aa72196ec47a
jkim427 <jkim428@student.cccd.edu>
2017-09-24 23:14:45 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/jkim428/inandout/OrderActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.cs273.jkim428.inandout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
